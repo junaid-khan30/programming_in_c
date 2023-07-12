7c4615feafe9405e207fe7e96d484c5149b015ba
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-07-12 21:48:48 +0530

    intro to c

--- intro/3.c
+++ intro/3.c
@@ -2,7 +2,7 @@
 int main(){
     int  num;
    printf("Guess your Number : \n");
-    // scanf("%d",&num);//its a function to accept inputs
-    printf("Your Guessed Number is %d\n",num);
-    return 0;
+     scanf("%d",&num);//its a function to accept inputs
+    //printf("Your Guessed Number is %d\n",num);
+    
 }
