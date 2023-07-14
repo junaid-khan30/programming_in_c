b632580c1505f0792b9735815ece2b62db045379
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-07-14 21:50:56 +0530

    intro

--- intro/3.c
+++ intro/3.c
@@ -9,9 +9,7 @@ int main(){
 
 /* 
 Header File : 
-main(): Every C Program has a primary function that must be named as main()
-The main() function acts as a starting point for the program execution 
-you can have n number of function , but the starting func is main()
+
 
 think of it as whenever we have a book , the context of that page is first one 
 
