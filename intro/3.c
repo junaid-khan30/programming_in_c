43c923123c72c234befd92d1b649b44baa81eba9
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-07-14 21:51:14 +0530

    intro

--- intro/3.c
+++ intro/3.c
@@ -10,10 +10,10 @@ int main(){
 /* 
 Header File : 
 
+main(): Every C Program has a primary function that must be named as main()
+The main() function acts as a starting point for the program execution 
+you can have n number of function , but the starting func is main()
 
-think of it as whenever we have a book , the context of that page is first one 
-
-for software folks , its very imp to know the entry point and end point 
 
 printf function is inportet from stdio.h 
 
