858037283b0c2f091dc02b10cb189b49275f0a01
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-07-14 21:50:17 +0530

    intro

--- intro/3.c
+++ intro/3.c
@@ -6,3 +6,23 @@ int main(){
     printf("Your Guessed Number is %d\n",num);
     return 0;
 }
+
+/* 
+Header File : 
+main(): Every C Program has a primary function that must be named as main()
+The main() function acts as a starting point for the program execution 
+you can have n number of function , but the starting func is main()
+
+think of it as whenever we have a book , the context of that page is first one 
+
+for software folks , its very imp to know the entry point and end point 
+
+printf function is inportet from stdio.h 
+
+its mandatory to name main() as main() only, for eg i name it as junaid, 
+what will happen it won't work 
+
+
+
+
+*/
