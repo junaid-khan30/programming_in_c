6f11766be6b34b83717d67eb8ff10e4960a66c7a
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-07-26 17:06:03 +0000

    intro

---
+++ intro/7.c
@@ -0,0 +1,13 @@
+//Take two numbers as an input 
+//and print those numbers as an output
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter First Number : \n");
+    scanf("%d",&num1);
+    printf("Enter Second Number : \n");
+    scanf("%d",&num2);
+    printf("Your First Number is %d and Your Second Number is %d\n",num1,num2);
+    
+    return 0;
+}
